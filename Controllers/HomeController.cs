b39a621dfece59d241792c0c6a6db5909a6020d3
PriyankaAryan <50867363+PriyankaAryan@users.noreply.github.com>
2020-12-14 11:20:35 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WebApplicationProject.Healper;
+using WebApplicationProject.Models;
+
+namespace WebApplicationProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        WordAPI _api = new WordAPI();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<WordData> words = new List<WordData>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/Word");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                words = JsonConvert.DeserializeObject<List<WordData>>(result);
+            }
+            return View(words);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/WordData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationProject.Models
+{
+    public class WordData
+    {
+        public int WordId { get; set; }
+        public string WordName { get; set; }
+
+    }
+}
